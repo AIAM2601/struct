78861201807471681bc5326d151c84c2a700e820
Andres <andres.aguilarfl@gmail.com>
2021-10-21 14:51:50 -0600

    update edges upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <gtkmm.h>
-#include <iostream>
+#include<gtkmm.h>
+#include<iostream>
+#include<list>
 
 using namespace std;
 using namespace Gtk;
@@ -11,6 +12,24 @@ class mainWindow : public Window
      
   protected: 
     void on_button_clicked();
+    void addEdge(vector<int> adj[], int u, int v)
+    {
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    void printGraph(vector<int> adj[], int V)
+    {
+      cout << "valor de grafo=> " << V << endl;
+
+      for (int v = 0; v < V; ++v)
+      {
+          cout << "\n Adjacency list of vertex "
+              << v << "\n head ";
+          for (auto x : adj[v])
+            cout << "-> " << x;
+          printf("\n");
+      }
+    }
     //void secondWindowMethod();
 
   private:
@@ -55,15 +74,34 @@ mainWindow::mainWindow()
   fixed.add(e_end);
   fixed.move(e_end, 110, 102);
 
-  s_button.signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_button_clicked));
   s_button.set_label("Search");
+  s_button.signal_button_release_event().connect([&](GdkEventButton*) {
+    //aqui se imprime toda la lista falta ahora que dependiendo de lo que sea se dibuje y cargar los datos en txt
+    int V = 5;
+    vector<int> adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    string start = e_start.get_text();
+    if (start == "asdf") {
+     printGraph(adj, V);
+    } else {
+      cout << "false";
+    }
+      
+      return true;
+    });
   fixed.add(s_button);
   fixed.move(s_button, 150, 150);
 
   img = Gtk::manage(new Gtk::Image());
   img->set("costaRica.jpeg");
   fixed.add(*img);
-  fixed.move(*img, 0, 200);     
+  fixed.move(*img, 0, 200);  
 
   show_all();
 }
@@ -77,79 +115,21 @@ mainWindow::mainWindow()
 // }
 
 /*************************************************/
-//grafo
-class Graph
-{
-    int V; 
-    map<pair<string,string>,int> city;
-    vector<string> city1;
-    vector<string> city2;
-    public:
-    Graph(int start_V)
-    {
-        V = start_V;
-    }
-    void addEdge(string start,string end,int weight);
-    void display();
-};
-
-void Graph::addEdge(string start,string end,int wt)
-{   
-   city[make_pair(start,end)] = wt;
-}
-void Graph::display()
-{   
-    int flag=0;
-    for(auto it:city)
-    {
-        city1.push_back(it.first.first);
-        city2.push_back(it.first.second);
-    }
-
-    sort(city1.begin(), city1.end()); 
-    auto last = unique(city1.begin(), city1.end());
-    city1.erase(last, city1.end());
-
-    sort(city2.begin(), city2.end()); 
-    auto last2 = unique(city2.begin(), city2.end());
-    city2.erase(last2, city2.end());
-
-    for(auto col:city2)
-    {
-        cout<<"\t"<<col;
-    }
-    cout<<"\n";
-
-    for(auto row:city1)
-    {
-    int flag =0;   //for printing row for once 
-    for(auto col:city2)
-    {
-        if (!flag)
-            cout<<row;
-        cout<<"\t"<<city[make_pair(row,col)];
-        flag = 1;
-    }
-
-    cout<<"\n";
-    }
-
-}
-
-/*************************************************/
+/************************************************
 
 
 void mainWindow::on_button_clicked() {
   string start = e_start.get_text();
   string end = e_end.get_text();
-  if (start == "San Jose")
+  if (start == "a")
   {
-    cout << "san jose";
+    cout << "a";
   } else 
   {
     cout << "error";
   }
-}
+
+}*/
 
 int main(int argc, char* argv[])
 {
